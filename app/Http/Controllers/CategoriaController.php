ea4dd0bbb28227d6204c2910373de2beece6f819
DarioLopezL <dariiolopez17@gmail.com>
2024-06-18 21:16:49 -0600

    Quiosco Product

---
+++ app/Http/Controllers/CategoriaController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Resources\CategoriaCollection;
+use App\Models\Categoria;
+use Illuminate\Http\Request;
+
+class CategoriaController extends Controller
+{
+    public function index(){
+
+        /* return response()->json(['categoria' => Categoria::all() ]); */
+        return new CategoriaCollection(Categoria::all());
+    }
+}
